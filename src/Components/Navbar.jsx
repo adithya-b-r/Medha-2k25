1e10834d967ebf7278ff1d1a119f9f694ec95e52
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2025-11-06 21:42:32 +0530

    fix: minor Navbar design tweaks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -52,7 +52,7 @@ export const Navbar = () => {
   }
 
   return (
-    <header className="text-white fixed top-0 left-0 right-0 z-20 bg-black/50 backdrop-blur-[0.5px]">
+    <header className="text-white fixed top-0 left-0 right-0 z-20 md:bg-black/10 bg-black/50 backdrop-blur-[0.5px]">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center p-5 mx-auto sm:px-10">
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors "><img className="h-12" src="/medha3.png" alt="" /></a>
@@ -67,7 +67,7 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className={`absolute left-0 right-0 bg-black/60 backdrop-blur-[10px] transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+      <div className={`absolute left-0 right-0 bg-black/50 backdrop-blur-[10px] transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
         <nav className="p-5">
           <NavItems isMobile={true} />
         </nav>
