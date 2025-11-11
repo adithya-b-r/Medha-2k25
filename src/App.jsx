c6137164abf5a97b543f13134ae1f3cc22d0aeaa
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2025-11-11 13:52:31 +0530

    fix: navbar build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import { Leva } from "leva";
-import { Navbar } from "./components/Navbar"
+import { Navbar } from "./components/Navbar";
 import { Hero } from "./sections/hero/Hero"
 import { About } from "./sections/About"
 import { Events } from "./sections/Events"
